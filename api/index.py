91a0bec2880b8d682ed2d11c2d826ec3dd74d1b4
BrotherHai <50613992+BrotherHai@users.noreply.github.com>
2023-10-22 20:59:14 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 from flask import Flask
 import feedparser
 from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 @app.route('/')
 def home():
